f59165f60fc2b0aa0b6caf9d7c21e187fa030065
gabel93 <jensgabel6@gmail.com>
2015-12-01 21:16:07 +0100

    import

--- src/main/java/database/SessionTool.java
+++ src/main/java/database/SessionTool.java
@@ -1,5 +1,13 @@
 package database;
 
+
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+
 public class SessionTool {
 
 }
