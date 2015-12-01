e93fc8c79b6a64711d9731205a9704225450d224
gabel93 <jensgabel6@gmail.com>
2015-12-01 21:17:04 +0100

    sessionTool

--- src/main/java/database/SessionTool.java
+++ src/main/java/database/SessionTool.java
@@ -7,7 +7,54 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.transaction.annotation.Transactional;
 
+public class SessionTool
+{
+    private final SessionFactory sessionFactory;
 
-public class SessionTool {
+    public SessionTool(SessionFactory sessionFactory)
+    {
+        this.sessionFactory = sessionFactory;
+    }
 
+    public Session getCurrentSession()
+    {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @Transactional
+    public void save(Object object)
+    {
+        Session session = this.getCurrentSession();
+        session.save(object);
+    }
+
+    @Transactional
+    public void update(Object object)
+    {
+        Session session = this.getCurrentSession();
+        session.update(object);
+    }
+    
+    @Transactional
+    public void saveOrUpdate(Object object)
+    {
+        Session session = this.getCurrentSession();
+        session.saveOrUpdate(object);
+    }
+
+    @Transactional
+    @SuppressWarnings("unchecked")
+    public <E> E get(Class<E> clazz, Serializable id)
+    {
+        Session session = this.getCurrentSession();
+        E element = (E) session.get(clazz, id);
+        return element;
+    }
+
+    @Transactional
+    public void delete(Object object)
+    {
+        Session session = this.getCurrentSession();
+        session.delete(object);
+    }
 }
