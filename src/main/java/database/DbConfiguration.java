b76c0fa710878b263798ccf132f6d8f008346c2a
gabel93 <jensgabel6@gmail.com>
2015-12-01 17:12:49 +0100

    password
    pass

--- src/main/java/database/DbConfiguration.java
+++ src/main/java/database/DbConfiguration.java
@@ -23,7 +23,7 @@ public class DbConfiguration
     public static final String DRIVER_CLASS_NAME = "driverClassName";
     public static final String URL = "url";
     public static final String USERNAME = "username";
-    public static final String PASSWORD = "password";
+    public static final String PASSWORD = "pass";
     
     @Bean
     public DataSource dataSource()
