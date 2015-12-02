ea2187c6deeaa7f648adb2d56897435546e45ddb
gabel93 <jensgabel6@gmail.com>
2015-12-02 14:20:14 +0100

    User - manøvre og funktioner

--- src/main/java/database/User.java
+++ src/main/java/database/User.java
@@ -1,5 +1,94 @@
 package database;
 
-public class User {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private boolean isAdmin;
+    @Column
+    private String loginKey;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getLoginKey()
+    {
+        return loginKey;
+    }
+
+    public void setLoginKey(String loginKey)
+    {
+        this.loginKey = loginKey;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public boolean isAdmin()
+    {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean isAdmin)
+    {
+        this.isAdmin = isAdmin;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
 }
