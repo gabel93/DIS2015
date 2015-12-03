b1f0235c9fda76f21c9a0c6773c14ff3b163ef72
gabel93 <jensgabel6@gmail.com>
2015-12-03 13:18:59 +0100

    import af dependencies

--- src/main/java/server/backend/ServiceConfiguration.java
+++ src/main/java/server/backend/ServiceConfiguration.java
@@ -1,5 +1,9 @@
 package server.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 public class ServiceConfiguration {
 
 }
