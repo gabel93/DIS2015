1f13700d2b912a37128552ab077e07f773f0503f
gabel93 <jensgabel6@gmail.com>
2015-12-03 13:21:10 +0100

    kode

--- src/main/java/server/backend/ServiceConfiguration.java
+++ src/main/java/server/backend/ServiceConfiguration.java
@@ -5,7 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import database.DbConfiguration;
-@Configuration
-public class ServiceConfiguration {
 
+@Configuration
+public class ServiceConfiguration 
+{
+	@Autowired
+    private DbConfiguration dbConfiguration;
+    
+    @Bean
+    public Services services()
+    {
+        return new Services(dbConfiguration.sessionTool());
+    }
 }
+
+
