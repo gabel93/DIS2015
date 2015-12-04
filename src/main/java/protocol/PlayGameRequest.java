6875e3b1d36c478b899eb53e42a04baedeb4578f
gabel93 <jensgabel6@gmail.com>
2015-12-04 10:59:06 +0100

    kommentar

--- src/main/java/protocol/PlayGameRequest.java
+++ src/main/java/protocol/PlayGameRequest.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class PlayGameRequest implements Serializable 
 {
+	//Har til formål at starte et spil. spillet skal dog være oprettet først
 	private String name;
 
     public String getName()
