06e6c94b84dbd5a775ac02a83af7cceabf4d85f8
gabel93 <jensgabel6@gmail.com>
2015-12-04 10:55:57 +0100

    kommentar

--- src/main/java/protocol/GetGameRequest.java
+++ src/main/java/protocol/GetGameRequest.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class GetGameRequest implements Serializable
 {
-	//Har til formål at hente spil i terminalen
+	//Har til formål at hente spil i terminalen, når man har åbnet en klient vel og mærke
     private String name;
 
     public String getName()
