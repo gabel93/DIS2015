1aee4aa826ec66a2d81bfae9211c64f18d8ec7c0
gabel93 <jensgabel6@gmail.com>
2015-12-04 10:50:17 +0100

    Play game funktionen

--- src/main/java/protocol/PlayGameRequest.java
+++ src/main/java/protocol/PlayGameRequest.java
@@ -4,5 +4,16 @@ import java.io.Serializable;
 
 public class PlayGameRequest implements Serializable 
 {
+	private String name;
 
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 }
+
