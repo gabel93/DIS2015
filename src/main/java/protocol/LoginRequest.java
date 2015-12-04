b32b89a81d86007f1f3aa7ff53f19f7ebb8d508a
gabel93 <jensgabel6@gmail.com>
2015-12-04 10:46:09 +0100

    login funktion. med password

--- src/main/java/protocol/LoginRequest.java
+++ src/main/java/protocol/LoginRequest.java
@@ -1,5 +1,29 @@
 package protocol;
 
-public class LoginRequest {
+import java.io.Serializable;
 
+public class LoginRequest implements Serializable
+{
+    private String username;
+    private String password;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 }
