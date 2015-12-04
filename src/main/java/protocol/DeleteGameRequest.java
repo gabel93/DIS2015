e6a4ec8e0de042dd0be74ad60b0e548d8475dd8e
gabel93 <jensgabel6@gmail.com>
2015-12-04 10:55:04 +0100

    kommentar

--- src/main/java/protocol/DeleteGameRequest.java
+++ src/main/java/protocol/DeleteGameRequest.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class DeleteGameRequest implements Serializable
 {
+	//Har til formål at skabe en funktion hvor der kan slettes spil fra databasen igennem terminalen,
     private String name;
 
     public String getName()
