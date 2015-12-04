0698540fdb69befa3db63c2a683e17c80e769168
gabel93 <jensgabel6@gmail.com>
2015-12-04 10:52:12 +0100

    kommando funktionen ( Set - Getter metode)

--- src/main/java/protocol/SetCommandsRequest.java
+++ src/main/java/protocol/SetCommandsRequest.java
@@ -4,5 +4,40 @@ import java.io.Serializable;
 
 public class SetCommandsRequest implements Serializable
 {
+	private String username;
+    private String gameName;
+    private String commands;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getGameName()
+    {
+        return gameName;
+    }
+
+    public void setGameName(String gameName)
+    {
+        this.gameName = gameName;
+    }
+
+    public String getCommands()
+    {
+        return commands;
+    }
+
+    public void setCommands(String commands)
+    {
+        this.commands = commands;
+    }
+
+}
 
 }
