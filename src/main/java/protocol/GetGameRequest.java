02efdab232639638c4ac0809cca6da860a37ec83
gabel93 <jensgabel6@gmail.com>
2015-12-04 10:39:37 +0100

    GetGame klassen

--- src/main/java/protocol/GetGameRequest.java
+++ src/main/java/protocol/GetGameRequest.java
@@ -1,5 +1,18 @@
 package protocol;
 
-public class GetGameRequest {
+import java.io.Serializable;
 
+public class GetGameRequest implements Serializable
+{
+    private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 }
