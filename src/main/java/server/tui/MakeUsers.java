34393088ef4441340b606a2a2abf2f3eabcc5761
gabel93 <jensgabel6@gmail.com>
2015-12-05 11:11:06 +0100

    lav brugere

--- src/main/java/server/tui/MakeUsers.java
+++ src/main/java/server/tui/MakeUsers.java
@@ -6,5 +6,15 @@ import server.backend.Services;
 import database.User;
 
 public class MakeUsers {
+	public static void main(String[] strings)
+    {
+        String[] names = {"", "", "", "", ""};
+        for(String name : names)
+        {
+            User user = new User();
+            user.setUsername(name);
+            user.setPassword(name);
+            Services.INSTANCE.add(user);
+        }
 
 }
