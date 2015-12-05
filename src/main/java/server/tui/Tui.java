60931e3abc31b3dd9b7f6a1e2e1a55104b7bd9af
gabel93 <jensgabel6@gmail.com>
2015-12-05 11:15:09 +0100

    login admin / username

--- src/main/java/server/tui/Tui.java
+++ src/main/java/server/tui/Tui.java
@@ -17,5 +17,29 @@ public class Tui {
     
     public void start()
     {
+    	while(true)
+        {
+            System.out.print("Admin username? [Admin]: ");
+            String username = scanner.nextLine();
+            if(username.length() == 0)
+            {
+                username = "Admin";
+            }
+            
+            System.out.print("Admin password? [password]: ");
+            String password = scanner.nextLine();
+            if(password.length() == 0)
+            {
+                password = "password";
+            }
+
+            User user = Services.INSTANCE.getUserByUsername(username);
+            if(user != null&& user.isAdmin()&& password.equals(user.getPassword()))
+            {
+                break;
+            }
+            System.out.println("This is not a valid admin.");
+        }
+    }
 }
    
