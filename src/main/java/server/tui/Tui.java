c21b128efb16a977e839ec224cbdbd2b1d3662d6
gabel93 <jensgabel6@gmail.com>
2015-12-05 11:16:03 +0100

    questions

--- src/main/java/server/tui/Tui.java
+++ src/main/java/server/tui/Tui.java
@@ -39,7 +39,21 @@ public class Tui {
                 break;
             }
             System.out.println("This is not a valid admin.");
+        }	
+    	
+    	questions:
+            while(true)
+            {
+                int option = this.readOption("1. Create a user 2. Delete a user 3. Create a game 4. Quit ", 1, 4);
+                switch(option)
+                {
+                case 1: this.createUser(); break;
+                case 2: this.deleteUser(); break;
+                case 3: this.createGame(); break;
+                default: break questions;
+                }
+            }
         }
-    }
-}
+    
+
    
