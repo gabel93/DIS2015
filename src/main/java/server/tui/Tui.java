e2766f09d0f6e8ff2aca901e916789633917267a
gabel93 <jensgabel6@gmail.com>
2015-12-05 11:16:43 +0100

    programmets svar på kommandoer

--- src/main/java/server/tui/Tui.java
+++ src/main/java/server/tui/Tui.java
@@ -55,5 +55,68 @@ public class Tui {
             }
         }
     
+    public void createUser()
+    {
+        User user = new User();
+        
+        System.out.print("Username? ");
+        user.setUsername(scanner.nextLine());
+        
+        System.out.print("Password? ");
+        user.setPassword(scanner.nextLine());
+        
+        Services.INSTANCE.add(user);
+        
+        System.out.println("User \"" + user.getUsername() + "\" has been created successfully.");
+    }
+    
+    public void deleteUser()
+    {
+        System.out.print("Username? ");
+        String username = scanner.nextLine();
+        
+        User user = Services.INSTANCE.getUserByUsername(username);
+        if(user == null)
+        {
+            System.out.println("No such user!");
+        }else
+        {
+            Services.INSTANCE.deleteUser(user.getId());
+            System.out.println("User " + user.getUsername() + " has been deleted.");
+        }
+    }
+    
+    public void createGame()
+    {
+        System.out.print("Name? ");
+        String name = scanner.nextLine();
+        
+        Game game = new Game();
+        game.setName(name);
+        
+        Services.INSTANCE.add(game);
+        System.out.println("Game " + name + " has been added successfully.");
+    }
+    
+    private int readOption(String question, int min, int max)
+    {
+        while(true)
+        {
+            System.out.print(question);
+            try
+            {
+                int option = Integer.parseInt(scanner.nextLine().trim());
+                if(option >= min&& option <= max)
+                {
+                    return option;
+                }
+            }catch(Exception e)
+            {
+                ;
+            }
+            System.out.println("Invalid input!");
+        }
+    }
+}
 
    
