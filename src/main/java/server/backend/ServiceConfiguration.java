ab05a1d5e59a621d8b6eac7c44ebea919ceeaf0d
gabel93 <jensgabel6@gmail.com>
2015-12-05 11:00:12 +0100

    - import

--- src/main/java/server/backend/ServiceConfiguration.java
+++ src/main/java/server/backend/ServiceConfiguration.java
@@ -3,7 +3,7 @@ package server.backend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.aop.*;
+
 
 
 import database.DbConfiguration;
