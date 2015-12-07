5d391ce9e5fcc5622ec498346e564dbe4bd6ea6e
gabel93 <jensgabel6@gmail.com>
2015-12-07 14:59:38 +0100

    ændr

--- src/main/java/client/Class.java
+++ src/main/java/client/Class.java
@@ -14,6 +14,7 @@ import protocol.HelloRequest;
 import protocol.LoginRequest;
 import protocol.LogoutRequest;
 import protocol.PlayGameRequest;
+import protocol.SetCommandsRequest;
 
 public class Class {
 
